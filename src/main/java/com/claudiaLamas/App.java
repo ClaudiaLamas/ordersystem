201cd2b0b81370f84924fe3126304f6d40f6a437
Claudia Lamas <claudia.c.lamas@gmail.com>
2025-09-01 01:13:24 +0100

    Initial commit

---
+++ src/main/java/com/claudiaLamas/App.java
@@ -0,0 +1,13 @@
+package com.claudiaLamas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App 
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(App.class, args);
+    }
+}
